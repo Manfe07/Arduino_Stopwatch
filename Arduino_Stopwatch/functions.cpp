e9f7d1cf67012cb7d1b5425bdcf6d8d546b131cc
Manuel Fehren <Manuel.Fehren@Gmail.com>
2019-04-12 21:49:34 +0200

    Created seperated function files for generic functions

---
+++ Arduino_Stopwatch/functions.cpp
@@ -0,0 +1,15 @@
+#include "functions.h"
+#include <Arduino.h>
+
+
+void debug(char _text[]){
+  Serial.print(millis());
+  Serial.print(" - ");
+  Serial.println(_text);
+}//END void debug(...)
+
+
+void wait(int _pin, int _time){
+  while(!digitalRead(_pin)){}
+  delay(_time);
+}//END void wait(...)
---
+++ Arduino_Stopwatch/functions.h
@@ -0,0 +1,7 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+void debug(char _text[]);
+void wait(int _pin, int _time);
+
+#endif
