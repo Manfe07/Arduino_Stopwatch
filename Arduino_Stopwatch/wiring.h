698c0086d30bb21241b1ca390ae4d5ce1919fb4f
Manuel Fehren <Manuel.Fehren@Gmail.com>
2019-05-02 22:01:53 +0200

    wiring examples

--- Arduino_Stopwatch/wiring.h
+++ Arduino_Stopwatch/wiring.h
@@ -1,12 +1,12 @@
 #ifndef WIRING_H
 #define WIRING_H
 
-#define Button_L1 0
-#define Button_L2 0
-#define Button_L3 0
-#define Button_R  0
+#define Button_L1   2
+#define Button_L2   3
+#define Button_L3   4
+#define Button_R    5
 
-#define RS485_Pin   0
+#define RS485_Pin   7
 
 #define Camera_Triger 0
 
